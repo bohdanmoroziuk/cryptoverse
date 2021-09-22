e2cbfa357d83f171c011f1d31db5ab2f3714c490
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-09-22 22:42:31 +0300

    feat(pages.home): display global crypto stats

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,59 @@
-const Home = () => (
-  <div className="page">
-    Home
-  </div>
-);
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+import millify from 'millify';
+
+import { useGetCryptosQuery } from 'services/crypto';
+
+const { Title } = Typography;
+
+const Home = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+
+  const stats = data?.data?.stats;
+
+  if (isFetching) {
+    return 'Loading...';
+  }
+
+  return (
+    <div className="page">
+      <Title className="heading" level={2}>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic 
+            title="Total Cryptocurrencies"
+            value={stats.total}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic 
+            title="Total Exchanges"
+            value={millify(stats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic 
+            title="Total Market Cap"
+            value={millify(stats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic 
+            title="Total 24h Volume"
+            value={millify(stats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic 
+            title="Total Markets"
+            value={millify(stats.totalMarkets)}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Home;
