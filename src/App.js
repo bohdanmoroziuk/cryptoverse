d4557fa98b81e82056b76e837861bc0e78466594
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-09-22 10:29:17 +0300

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Space, Typography } from 'antd';
+
+import { Navbar } from 'components';
+
+import Logo from 'assets/images/cryptocurrency.png';
+
 const App = () => (
   <div className="app">
-    Cryptoverse
+    <nav className="navbar">
+      <Navbar icon={Logo} title="Cryptoverse" />
+    </nav>
+    <main className="main"></main>
+    <footer className="footer"></footer>
   </div>
 );
 
