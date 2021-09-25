733a458363aa74f1a2d61faee983764e184d9409
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-09-25 14:15:58 +0300

    feat(navbar): add mobile navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { Menu, Button, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import {
@@ -8,29 +10,63 @@ import {
   MenuOutlined,
 } from '@ant-design/icons';
 
-const Navbar = ({ title, icon }) => (
-  <div className="nav-container">
-    <div className="logo-container">
-      <Avatar src={icon} size="large" />
-      <Typography.Title className="logo" level={2}>
-        <Link to="/">{title}</Link>
-      </Typography.Title>
+const Navbar = ({ title, icon }) => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  
+  const [screenSize, setScreenSize] = useState(null);
+
+  const toggleMenu = () => {
+    setIsMenuActive((isMenuActive) => !isMenuActive);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsMenuActive(screenSize >= 768);
+  }, [screenSize]);
+
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title className="logo" level={2}>
+          <Link to="/">{title}</Link>
+        </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={toggleMenu}
+        >
+          <MenuOutlined />
+        </Button>
+      </div>
+      {isMenuActive && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />} key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />} key="cryptocurrencies">
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />} key="news">
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
-    <Menu theme="dark">
-      <Menu.Item icon={<HomeOutlined />} key="home">
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item icon={<FundOutlined />} key="cryptocurrencies">
-        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-      </Menu.Item>
-      <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
-        <Link to="/exchanges">Exchanges</Link>
-      </Menu.Item>
-      <Menu.Item icon={<BulbOutlined />} key="news">
-        <Link to="/news">News</Link>
-      </Menu.Item>
-    </Menu>
-  </div>
-);
+  );
+};
 
 export default Navbar;
