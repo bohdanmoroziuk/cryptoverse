7e93e4e09fee269167f62b883c55fafa08a10888
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-09-27 21:45:17 +0300

    feat: scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -3,56 +3,59 @@ import { Layout, Space, Typography } from 'antd';
 
 import { Home, News, Exchanges, Cryptocurrencies, Crypto } from 'pages';
 
-import { Navbar } from 'components';
+import { Navbar, ScrollToTop } from 'components';
 
 import Logo from 'assets/images/cryptocurrency.png';
 
 const App = () => (
-  <div className="app">
-    <nav className="navbar">
-      <Navbar icon={Logo} title="Cryptoverse" />
-    </nav>
-    <main className="main">
-      <Layout>
-        <div className="routes">
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/news">
-              <News />
-            </Route>
-            <Route path="/exchanges">
-              <Exchanges />
-            </Route>
-            <Route path="/crypto/:coinId">
-              <Crypto />
-            </Route>
-            <Route path="/cryptocurrencies">
-              <Cryptocurrencies />
-            </Route>
-          </Switch>
-        </div>
-      </Layout>
-      <footer className="footer">
-        <Typography.Title
-          level={5}
-          style={{ color: 'white', textAlign: 'center' }}  
-        >
-          Cryptoverse
-        </Typography.Title>
-        <Typography.Text style={{ color: 'white', textAlign: 'center' }}>
-          All rights reserved
-        </Typography.Text>
-        <Space>
-          <Link to="/">Home</Link>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          <Link to="/exchanges">Exchanges</Link>
-          <Link to="/news">News</Link>
-        </Space>
-      </footer>
-    </main>
-  </div>
+  <>
+    <ScrollToTop />
+    <div className="app">
+      <nav className="navbar">
+        <Navbar icon={Logo} title="Cryptoverse" />
+      </nav>
+      <main className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/news">
+                <News />
+              </Route>
+              <Route path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route path="/crypto/:coinId">
+                <Crypto />
+              </Route>
+              <Route path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <footer className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}  
+          >
+            Cryptoverse
+          </Typography.Title>
+          <Typography.Text style={{ color: 'white', textAlign: 'center' }}>
+            All rights reserved
+          </Typography.Text>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </footer>
+      </main>
+    </div>
+  </>
 );
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,8 @@
+export { default as Loader } from 'components/Loader';
 export { default as Navbar } from 'components/Navbar';
-
 export { default as NewsCard } from 'components/NewsCard';
 export { default as NewsList } from 'components/NewsList';
-
+export { default as LineChart } from 'components/LineChart';
+export { default as ScrollToTop } from 'components/ScrollToTop';
 export { default as CryptocurrencyCard } from 'components/CryptocurrencyCard';
 export { default as CryptocurrenciesList } from 'components/CryptocurrenciesList';
-
-export { default as Loader } from 'components/Loader';
-
-export { default as LineChart } from 'components/LineChart';
